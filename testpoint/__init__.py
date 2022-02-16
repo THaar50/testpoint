66a853a594cdceceee6152089b0bdf1f551932fc
THaar50 <haar.tobias@gmail.com>
2022-02-17 00:53:11 +0100

    docstrings and todo added

--- testpoint/__init__.py
+++ testpoint/__init__.py
@@ -4,18 +4,19 @@ from secrets import token_urlsafe
 
 db = SQLAlchemy()
 
-# DB_USERNAME = os.environ.get('TESTPOINT_DB_USERNAME')
-# DB_PASSWORD = os.environ.get('TESTPOINT_DB_PASSWORD')
-# DB_NAME = os.environ.get('TESTPOINT_DB_NAME')
-# DB_PORT = os.environ.get('TESTPOINT_DB_PORT')
+# TODO DB_USERNAME = os.environ.get('TESTPOINT_DB_USERNAME')
+# TODO DB_PASSWORD = os.environ.get('TESTPOINT_DB_PASSWORD')
+# TODO DB_NAME = os.environ.get('TESTPOINT_DB_NAME')
+# TODO DB_PORT = os.environ.get('TESTPOINT_DB_PORT')
 
 
 def create_app() -> Flask:
+    """Creates a Flask application setting its secret and linking it with the database"""
     app = Flask(__name__)
     app.config['SECRET_KEY'] = token_urlsafe(nbytes=256)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    # app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@localhost:{DB_PORT}/{DB_NAME}'
-    # db.init_app(app=app)
+    # TODO app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@localhost:{
+    #  DB_PORT}/{DB_NAME}' TODO db.init_app(app=app)
 
     from .views import views
 
