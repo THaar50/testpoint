4066a931f5ebd19aae5582af9bd45d79d0f4d895
THaar50 <haar.tobias@gmail.com>
2022-02-16 13:39:12 +0100

    set flask secret

--- testpoint/__init__.py
+++ testpoint/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
+from secrets import token_urlsafe
 
 db = SQLAlchemy()
 
@@ -12,7 +12,7 @@ db = SQLAlchemy()
 
 def create_app() -> Flask:
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = os.environ.get('TESTPOINT_UI_SECRET_KEY')
+    app.config['SECRET_KEY'] = token_urlsafe(nbytes=256)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     # app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@localhost:{DB_PORT}/{DB_NAME}'
     # db.init_app(app=app)
