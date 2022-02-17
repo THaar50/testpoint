4df40cf065f24a0630b74e315aaa807657b832bb
THaar50 <haar.tobias@gmail.com>
2022-02-17 02:07:58 +0100

    added mysql database

--- testpoint/__init__.py
+++ testpoint/__init__.py
@@ -1,13 +1,17 @@
+from configparser import ConfigParser
+from secrets import token_urlsafe
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from secrets import token_urlsafe
 
 db = SQLAlchemy()
 
-# TODO DB_USERNAME = os.environ.get('TESTPOINT_DB_USERNAME')
-# TODO DB_PASSWORD = os.environ.get('TESTPOINT_DB_PASSWORD')
-# TODO DB_NAME = os.environ.get('TESTPOINT_DB_NAME')
-# TODO DB_PORT = os.environ.get('TESTPOINT_DB_PORT')
+config = ConfigParser()
+config.read('config.ini')
+DB_USER = config['DATABASE']['DB_USER']
+DB_PW = config['DATABASE']['DB_PW']
+DB_ADDRESS = config['DATABASE']['DB_ADDRESS']
+DB_PORT = config['DATABASE']['DB_PORT']
+DB_NAME = config['DATABASE']['DB_NAME']
 
 
 def create_app() -> Flask:
@@ -15,8 +19,8 @@ def create_app() -> Flask:
     app = Flask(__name__)
     app.config['SECRET_KEY'] = token_urlsafe(nbytes=256)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    # TODO app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@localhost:{
-    #  DB_PORT}/{DB_NAME}' TODO db.init_app(app=app)
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{DB_USER}:{DB_PW}@{DB_ADDRESS}:{DB_PORT}/{DB_NAME}'
+    db.init_app(app=app)
 
     from .views import views
 
