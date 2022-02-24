0c491c986b61bdf94095e66914a069142b67241a
THaar50 <haar.tobias@gmail.com>
2022-02-25 00:43:49 +0100

    add config module

---
+++ testpoint/config.py
@@ -0,0 +1,14 @@
+from configparser import ConfigParser
+
+config = ConfigParser()
+config.read('config.ini')
+DB_USER = config['DATABASE']['USER']
+DB_PW = config['DATABASE']['PW']
+DB_ADDRESS = config['DATABASE']['ADDRESS']
+DB_PORT = config['DATABASE']['PORT']
+DB_NAME = config['DATABASE']['NAME']
+
+EMAIL_USER = config['EMAIL']['USER']
+EMAIL_PW = config['EMAIL']['PW']
+EMAIL_SERVER = config['EMAIL']['SERVER']
+EMAIL_PORT = config['EMAIL']['PORT']
